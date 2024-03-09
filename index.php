89a94903643607f5fb7f1c5846da74c2025b2bf5
Jorge <jorglo03@ucm.es>
2024-03-09 14:13:53 +0100

    creada la base

--- index.php
+++ index.php
@@ -13,22 +13,5 @@
         require 'vistas/cabecera.php';
 
     ?>
-    <nav class="navbar">
-        <ul class="nav-links">
-            <li><a href="index.html">Inicio</a></li>
-            <li><a href="bocetos.html">Bocetos</a></li>
-            <li><a href="detalles.html">Detalles</a></li>
-            <li><a href="miembros.html">Miembros</a></li>
-            <li><a href="planificacion.html">Planificacion</a></li>
-            <li><a href="contacto.html">Contacto</a></li>
-        </ul>
-    </nav>
-    <h1>GameForum!</h1>
-    <p>Nuestro objetivo es unir a la comunidad gamer mediante un sistema web que funciona a modo de foro.</p>
-    <p>Será una plataforma en la cual los distintos usuarios puedan calificar, opinar y debatir sobre cualquier juego. De tal manera, antes de decidir jugar a cualquier videojuego, podrás ver su valoración, duración, encontrar trucos o atajos concretos entre muchas otras cosas que haya compartido la comunidad.</p>
-    <p>Nuestra página también tendrá implementado un apartado con noticias, lanzamientos y polémicas relacionadas con el sector de los videojuegos para mantenerte al día.</p>
-    <p>Se podrán sugerir y dar de alta videojuegos por parte de usuarios registrados, de manera que esos sean luego los videojuegos que podrán ser valorados y criticados.</p>
-    <p>Los tipos de usuarios que habrá serán usuarios no registrados, usuarios registrados, expertos, moderadores y administradores.</p>
-    <img src="fotos/logo.jpg" alt="Foto del proyecto" width="350" height="250">
 </body>
 </html>
--- vistas/cabecera.php
+++ vistas/cabecera.php
@@ -3,9 +3,37 @@
 require_once 'vistas/helpers/usuarios.php';
 
 ?>
-<header>
-	<h1>Mi gran página web</h1>
-	<div class="saludo">
-	  <?= saludo() ?>
-	</div>
-</header>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi gran página web</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <h1>GameForum!</h1>
+            </div>
+            <div class="login">
+				<?= saludo() ?>
+            </div>
+        </div>
+    </header>
+
+    <nav>
+        <div class="container">
+            <ul>
+                <li><a href="#">Foro</a></li>
+                <li><a href="#">Noticias</a></li>
+                <li><a href="#">Top Juegos</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- El resto del contenido de tu página va aquí -->
+
+</body>
+</html>
