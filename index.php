a1b946fa4a1f2bfd2f56439d197a850829371a17
Jose <galvez.jose.tpr.3a@gmail.com>
2024-04-12 20:25:08 +0200

    Corregido el index(otra vez)

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ require_once 'config.php';
 $tituloPagina = 'GameForum!';
 
 $contenidoPrincipal = <<<EOS
-
+<div class="bienvenida">
+<h2>Bienvenido a GameForum!</h2>
+<p>¡Aquí encontrarás todo sobre tus juegos favoritos, noticias de la industria del videojuego y mucho más!</p>
+</div>
 EOS;
 
 require 'vistas/comun/layout.php';
